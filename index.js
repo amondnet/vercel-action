29e3c4754109ef6908296d99b3eeb820160363c5
iam4x <iam4x@me.com>
2019-08-26 14:08:54 +0200

    feat: list comments

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const { Toolkit } = require('actions-toolkit')
 Toolkit.run(async tools => {
   tools.log.info(JSON.stringify(tools.context, null, 4));
 
-  await tools.github.issues.createComment({
+  const params = {
     owner: tools.context.repo.owner,
     repo: tools.context.repo.repo,
     issue_number: tools.context.payload.pull_request.number,
+  };
+
+  const comments = await tools.github.issues.listComments(params);
+  tools.log.info(comments);
+
+  await tools.github.issues.createComment({
+    ...params,
     body: 'Auto Comment Test',
   });
 
