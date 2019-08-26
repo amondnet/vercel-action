224270736de93e2478a65fbcaf806cfef2da687a
iam4x <iam4x@me.com>
2019-08-26 14:43:05 +0200

    feat: get deployment url for commit

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
-const axios = require('axios');
-const { stripIndents } = require('common-tags');
-const { Toolkit } = require('actions-toolkit')
+const axios = require("axios");
+const { stripIndents } = require("common-tags");
+const { Toolkit } = require("actions-toolkit");
 
 if (!process.env.ZEIT_TOKEN) {
   throw new Error(`ZEIT_TOKEN environment variable is not set`);
 }
 
 const zeitAPIClient = axios.create({
-  baseURL: 'https://api.zeit.co',
+  baseURL: "https://api.zeit.co",
   headers: {
-    'Authorization': `Bearer ${process.env.ZEIT_TOKEN}`
+    Authorization: `Bearer ${process.env.ZEIT_TOKEN}`
+  },
+  params: {
+    teamId: process.env.ZEIT_TEAMID || undefined,
+    "meta-commit": 'd6200a8bf4bd77d1d35e13e174dcc5f30bee85e5'
   }
-})
+});
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
   const { data: comments } = await tools.github.issues.listComments({
-    owner: tools.context.repo.owner,
-    repo: tools.context.repo.repo,
+    ...repo.owner,
     issue_number: tools.context.payload.pull_request.number
   });
 
-  const zeitPreviewURLComment = comments.find(
-    comment => comment.body.startsWith('Deploy preview for _website_ ready!')
+  const zeitPreviewURLComment = comments.find(comment =>
+    comment.body.startsWith("Deploy preview for _website_ ready!")
   );
+
+  const {
+    data: {
+      deployments: [deployment]
+    }
+  } = await zeitAPIClient.get("/v4/now/deployments");
 
   const commentBody = stripIndents`
     Deploy preview for _website_ ready!
 
     Build with commit ${process.env.GITHUB_SHA}
 
-    https://preview.mobile.club
-  `
+    https://${deployment.url}
+  `;
 
   if (zeitPreviewURLComment) {
     await tools.github.issues.updateComment({
+      ...repo.owner,
       id: zeitPreviewURLComment.id,
-      owner: tools.context.repo.owner,
-      repo: tools.context.repo.repo,
       body: commentBody
-    })
+    });
   } else {
     await tools.github.issues.createComment({
-      owner: tools.context.repo.owner,
-      repo: tools.context.repo.repo,
+      ...tools.context.repo,
       issue_number: tools.context.payload.pull_request.number,
       body: commentBody
     });
   }
 
-
-  tools.exit.success('We did it!');
+  tools.exit.success("We did it!");
 });
