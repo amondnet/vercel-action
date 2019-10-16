f91b7595a92cd35d3f956d3ece25eb28cfb7cd6d
Minsu Lee <amond@amond.net>
2019-10-16 20:08:39 +0900

    actions

--- index.js
+++ index.js
@@ -1,80 +1,78 @@
-const axios = require("axios");
-const { stripIndents } = require("common-tags");
-const core = require('@actions/core');
-const github = require('@actions/github');
-const context = github.context;
+const axios = require('axios')
+const { stripIndents } = require('common-tags')
+const core = require('@actions/core')
+const github = require('@actions/github')
+const context = github.context
 
-const zeitToken = core.getInput('zeit-token');
-const zeitTeamId = core.getInput('zeit-team-id');
-const githubToken = core.getInput('github-token');
+const zeitToken = core.getInput('zeit-token')
+const zeitTeamId = core.getInput('zeit-team-id')
+const githubToken = core.getInput('github-token')
 
 const zeitAPIClient = axios.create({
-  baseURL: "https://api.zeit.co",
-  headers: {
-    Authorization: `Bearer ${zeitToken}`
+  baseURL: 'https://api.zeit.co', headers: {
+    Authorization: `Bearer ${zeitToken}`,
+  }, params: {
+    teamId: zeitTeamId || undefined,
   },
-  params: {
-    teamId: zeitTeamId || undefined
-  }
-});
+})
 
-const octokit = new github.GitHub(githubToken);
+const octokit = new github.GitHub(githubToken)
 
-async function run() {
-  const comments = await octokit.issues.listComments({
-    ...context.repo,
-    issue_number: context.payload.pull_request.number
-  });
-  console.log(comments);
+async function run () {
+  const {
+    data: comments,
+  } = await octokit.issues.listComments({
+    ...context.repo, issue_number: context.payload.pull_request.number,
+  })
+  console.log(comments)
 
-  const zeitPreviewURLComment = comments.find(comment =>
-    comment.body.startsWith("Deploy preview for _website_ ready!")
-  );
+  const zeitPreviewURLComment = comments.find(
+    comment => comment.body.startsWith('Deploy preview for _website_ ready!'))
 
-  let deploymentUrl;
-  let deploymentCommit;
+  let deploymentUrl
+  let deploymentCommit
 
   const {
     data: {
-      deployments: [commitDeployment]
-    }
-  } = await zeitAPIClient.get("/v4/now/deployments", {
+      deployments: [commitDeployment],
+    },
+  } = await zeitAPIClient.get('/v4/now/deployments', {
     params: {
-      "meta-commit": context.sha
-    }
-  });
+      'meta-commit': context.sha,
+    },
+  })
 
   if (commitDeployment) {
-    deploymentUrl = commitDeployment.url;
-    deploymentCommit = commitDeployment.meta.commit;
+    deploymentUrl = commitDeployment.url
+    deploymentCommit = commitDeployment.meta.commit
   } else {
     const {
       data: {
-        deployments: [lastBranchDeployment]
-      }
-    } = await zeitAPIClient.get("/v4/now/deployments", {
+        deployments: [lastBranchDeployment],
+      },
+    } = await zeitAPIClient.get('/v4/now/deployments', {
       params: {
-        "meta-branch": context.ref
-      }
-    });
+        'meta-branch': context.ref,
+      },
+    })
 
     if (lastBranchDeployment) {
-      deploymentUrl = lastBranchDeployment.url;
-      deploymentCommit = lastBranchDeployment.meta.commit;
+      deploymentUrl = lastBranchDeployment.url
+      deploymentCommit = lastBranchDeployment.meta.commit
     } else {
       const {
         data: {
-          deployments: [lastDeployment]
-        }
-      } = await zeitAPIClient.get("/v4/now/deployments", {
+          deployments: [lastDeployment],
+        },
+      } = await zeitAPIClient.get('/v4/now/deployments', {
         params: {
-          limit: 1
-        }
-      });
+          limit: 1,
+        },
+      })
 
       if (lastDeployment) {
-        deploymentUrl = lastDeployment.url;
-        deploymentCommit = lastDeployment.meta.commit;
+        deploymentUrl = lastDeployment.url
+        deploymentCommit = lastDeployment.meta.commit
       }
     }
   }
@@ -85,26 +83,23 @@ async function run() {
     Built with commit ${deploymentCommit}
 
     https://${deploymentUrl}
-  `;
+  `
 
   if (zeitPreviewURLComment) {
     await octokit.issues.updateComment({
-      ...context.repo,
-      comment_id: zeitPreviewURLComment.id,
-      body: commentBody
-    });
+      ...context.repo, comment_id: zeitPreviewURLComment.id, body: commentBody,
+    })
   } else {
     await octokit.issues.createComment({
       ...context.repo,
       issue_number: context.payload.pull_request.number,
-      body: commentBody
-    });
+      body: commentBody,
+    })
   }
-  core.setOutput('preview-url', `https://${deploymentUrl}`);
+  core.setOutput('preview-url', `https://${deploymentUrl}`)
 }
 
-
-run().catch( error => {
-  core.setFailed(error.message);
-});
+run().catch(error => {
+  core.setFailed(error.message)
+})
 
