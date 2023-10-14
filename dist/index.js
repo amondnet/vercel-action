8433ae13f35aa62d68184a8fc369ce6b0143b0a3
Skyf0l <tom.rorato@epitech.eu>
2023-10-14 13:33:03 +0200

    chore: package script (#238)

--- dist/index.js
+++ dist/index.js
@@ -32906,7 +32906,6 @@ async function aliasDomainsToDeployment(deploymentUrl) {
 }
 
 async function run() {
-  core.info('Does it really run ????????????????????????');
   core.debug(`action : ${context.action}`);
   core.debug(`ref : ${context.ref}`);
   core.debug(`eventName : ${context.eventName}`);
